716869519dd137effbca51b81e20bbf45217e434
mdashmiller <m.robert.miller@gmail.com>
2018-10-24 21:38:10 -0500

    added contact form, footer, and social icons in nav

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
+	// Material css functionality
 	$('.sidenav').sidenav()
 	$('.parallax').parallax()
+	$('.tooltipped').tooltip()
+
+	// prevent form submit from reloading page
+	$('.btn').on('click', e =>
+		e.preventDefault()
+	)
 })
