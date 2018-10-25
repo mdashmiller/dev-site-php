585a8df82daf92ea293d01417078a4e7aac885a4
mdashmiller <m.robert.miller@gmail.com>
2018-10-25 15:03:06 -0500

    added fixed mini-nav for desktops

--- public/script.js
+++ public/script.js
@@ -5,6 +5,28 @@ $(document).ready(() => {
 	$('.tooltipped').tooltip()
 	$('.scrollspy').scrollSpy()
 
+	// hide mini-nav when user is at top of window
+	$(window).scroll(() => {
+		if ($(window).scrollTop() !== 0) {
+			$('#mini-nav-btn').addClass('visible')
+		} else {
+			$('#mini-nav-btn').removeClass('visible')
+		}
+	})
+
+	// show and hide mini-nav on mouseover
+	$('#mini-nav-btn').hover(() =>
+		$('#mini-nav-menu').addClass('visible')
+	)
+	$('#mini-nav-menu').mouseleave(() =>
+		$('#mini-nav-menu').removeClass('visible')
+	)
+
+	// hide mini-nav when a menu item is selected
+	$('#mini-nav-menu').on('click', () =>
+		$('#mini-nav-menu').removeClass('visible')
+	)
+
 	// prevent form submit from reloading page
 	$('.btn').on('click', e =>
 		e.preventDefault()
