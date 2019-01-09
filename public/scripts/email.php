9177650a511f1fc5d795d5e54eacdf4d0dbc3f91
mdashmiller <m.robert.miller@gmail.com>
2019-01-09 16:15:03 -0600

    ajax to handle form

---
+++ public/scripts/email.php
@@ -0,0 +1,17 @@
+<?php
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	echo $message . " was sent from " . $email;
+	// if (isset($_POST['submit'])) {
+	// 	$email = $_POST['email'];
+	// 	$subject = "a message from me.com";
+	// 	$message = $_POST['message'];
+
+	// 	$mailTo = "m.robert.miller@gmail.com";
+	// 	$headers = "From: " . $email;
+
+	// 	mail($mailTo, $subject, $message, $headers);
+	// 	header("Location: index.html?mail-sent");
+	// }
+?>
